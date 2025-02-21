4159a8b89b55aae0a4736026f453441d33ee1aa2
pratick13 <pratick.bhowmick.official@gmail.com>
2025-02-21 21:51:45 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# dataflow
+# SyncBridge
---
+++ src/main/java/com/syncbridge/schemamanager/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.syncbridge.schemamanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
